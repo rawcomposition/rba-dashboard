03868ce1e323e84d51cc6881369f4e1d28e3fa08
Adam Jackson <akjackson1@gmail.com>
2026-03-30 14:06:32 -0700

    Update alerts.ts

--- lib/alerts.ts
+++ lib/alerts.ts
@@ -22,15 +22,6 @@ export type AlertConfig = RegionAlertConfig | RadiusAlertConfig;
 
 export const alerts: AlertConfig[] = [
   {
-    id: "lower48",
-    name: "Lower 48",
-    type: "region",
-    back: 1,
-    regionCode: "US",
-    excludeSubRegions: ["US-HI", "US-AK"],
-    lifelistDownloadUrl: "https://ebird.org/lifelist?r=US&time=life&fmt=csv",
-  },
-  {
     id: "7.5mr",
     name: "7.5 MR",
     type: "radius",
@@ -38,5 +29,14 @@ export const alerts: AlertConfig[] = [
     lat: 33.58406395653916,
     lng: -117.18464387510173,
     dist: 12.07, // km
+  },
+  {
+    id: "lower48",
+    name: "Lower 48",
+    type: "region",
+    back: 1,
+    regionCode: "US",
+    excludeSubRegions: ["US-HI", "US-AK"],
+    lifelistDownloadUrl: "https://ebird.org/lifelist?r=US&time=life&fmt=csv",
   },
 ];
